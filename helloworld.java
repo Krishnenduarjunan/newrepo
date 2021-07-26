44aaed86e9fa60435f47ab8e87c674897f424804
krish <Krishnendu.Arjunan@ust.com>
2021-07-26 14:23:36 +0530

    new file added

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,8 @@
-class helloworld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
-    }
+class HelloWorld {
+	    
+	public static void main(String[] args) {
+		            
+		System.out.println("Hello, World!"); 
+			   
+}
 }
