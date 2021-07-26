b4f76b2e7c296d76546af43349cb25b4b8a3db84
Krishnenduarjunan <87644114+Krishnenduarjunan@users.noreply.github.com>
2021-07-26 14:08:26 +0530

    Create DiamondPattern.java

---
+++ DiamondPattern.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class DiamondPattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+	    System.out.print("Input number of rows: ");
+	    int n=sc.nextInt();
+	    int space= n - 1;
+		for (int i = 0; i < n; i++)
+        {
+            // loop for initially space,
+            for (int j = 0; j < space; j++)
+                System.out.print(" ");
+     
+            // Print i+1 stars
+            for (int j = 0; j <= i; j++)
+                System.out.print("* ");
+     
+            System.out.print("\n");
+            space--;
+        }
+		
+		space=0;
+		//reverse order
+		for (int i = n; i > 0; i--)
+        {
+            // loop for initially space,
+            for (int j = 0; j < space; j++)
+                System.out.print(" ");
+     
+            // Print i stars
+            for (int j = 0; j < i; j++)
+                System.out.print("* ");
+     
+            System.out.print("\n");
+            space++;
+        }
+	}
+
+}
+  
+
+
+
+
+
